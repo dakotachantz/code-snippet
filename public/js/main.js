3afaa426ee90826198d3b697f167a28c7a51e455
Dakota Nichols <dakotachantz@gmail.com>
2017-09-06 18:33:58 -0500

    added /js/main.js for front end js that was in html. installed jest and supertest

---
+++ public/js/main.js
@@ -0,0 +1,27 @@
+function showSnippetForm() {
+    event.preventDefault();
+    let form = document.getElementById("createSnippetForm");
+    if (form.style.display == 'block') {
+        form.style.display = "none";
+    } else {
+        form.style.display = "block";
+    }
+}
+function showFullSnippet() {
+    let viewSnippet = event.target;
+    let viewSnippetID = viewSnippet.getAttribute('data-id');
+    console.log(viewSnippetID);
+    let snippet = document.getElementById('item_' + viewSnippetID);
+    let cover = document.getElementById('cover_' + viewSnippetID);
+    console.log(snippet);
+
+    if (snippet.style.height !== '100%') {
+        snippet.style.height = '100%';
+        viewSnippet.textContent = "Hide Snippet";
+        cover.style.display = 'none';
+    } else {
+        snippet.style.height = '20em';
+        viewSnippet.textContent = "View Snippet";
+        cover.style.display = 'block';
+    }
+}
